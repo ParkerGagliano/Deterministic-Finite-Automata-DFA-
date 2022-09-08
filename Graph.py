7ac02d71214439c142d620c090368348eb25b05f
Parker <pjg4086@uncw.edu>
2022-09-07 21:20:17 -0400

    final touches, still needs explicit input control if needed

--- Graph.py
+++ Graph.py
@@ -1,41 +1,43 @@
 import time
+
+
 class Graph:
     def __init__(self, graph, mappings, start, final):
         self.graph = graph
-        self.mappings=mappings
+        self.mappings = mappings
         self.start = start
         self.final = final
-        self.dic={}
+        self.dic = {}
 
     def createMapping(self):
         for i in range(len(self.mappings)):
-            self.dic[self.mappings[i][0]]=self.mappings[i][1]
+            self.dic[self.mappings[i][0]] = self.mappings[i][1]
 
     def judge(self, string):
         current = self.start
         for i in string:
             current = self.dic[current+i]
         if current == self.final:
-            print('Works')
+            print('\nLast node is final node, string is accepted')
             return True
         else:
-            print('Doesnt accept')
+            print('\nLast node does not match the final node, string is not accepted')
             return False
-        
+
     def visualizedJudge(self, string):
         current = self.start
-        print(f"Judging DFA, starting at {current}")
+        print(f"\nJudging DFA, starting at {current}")
         time.sleep(.5)
         for i in string:
-            tempcurrent = current 
+            tempcurrent = current
             current = self.dic[current+i]
-            print(f"{tempcurrent} with {i} as input brings us to {current}")
+            print(f"\n{tempcurrent} with {i} as input brings us to {current}")
             time.sleep(.5)
-        print(f"that was the last comparison, is {current} the final state?")
+        print(f"\nthat was the last comparison, is {current} the final state?")
         time.sleep(.5)
         if current == self.final:
-            print('Yes it is, this string would be accepted!')
+            print('\nYes it is, this string would be accepted!')
             return True
         else:
-            print('No its not, this string is denied!')
+            print('\nNo its not, this string is denied!')
             return False
--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 from Graph import Graph
 
+
 def main():
-    print('---DFA Solver, please specify the input---')
-    print('example input is q0 q1 q2, with only 1 space in between')
+    print('\n---DFA Solver, please specify the input---')
+    print('\nexample input is q0 q1 q2, with only 1 space in between')
     inp = input()
     graph = inp.split()
     temp = []
     for i in range(2):
         for x in range(len(graph)):
             #print(f'specify the {i}th mapping of this DFA:')
-            print(f'--what does q{x}, {i} map to? ex q0--')
+            print(f'\n--what does q{x}, {i} map to? ex q0--')
             inp = input()
             temp.append([f'q{x}{i}', f'{inp}'])
-    print("--what is the inital state? ex q0--")
+    print("\n--what is the inital state? ex q0--")
     start = input()
-    print("--Lastly what is the final state ex q0--")
+    print("\n--Lastly what is the final state ex q0--")
     final = input()
     theGraph = Graph(graph, temp, start, final)
     theGraph.createMapping()
-    print("--graph is created--")
-    print("what string would you like to try with this DFA? ex: 1010101")
-    theString=input()
-    print("would you like to see the process of the DFA evaluation y/n?")
+    print("\n--graph is created--")
+    print("\nwhat string would you like to try with this DFA? ex: 1010101")
+    theString = input()
+    print("\nwould you like to see the process of the DFA evaluation y/n?")
     yn = input()
     if yn == 'y':
         theGraph.visualizedJudge(theString)
@@ -31,7 +32,5 @@ def main():
         'wrong input'
 
 
-
 if __name__ == "__main__":
     main()
-
